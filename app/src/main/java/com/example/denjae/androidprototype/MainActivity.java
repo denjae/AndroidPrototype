5e5e1a4f8fddfb340469bc98acecef4ac07c5c55
denjae <dennis.jga@gmail.com>
2014-07-25 14:39:05 +0200

    onClickListener implement, ExceptionHandling at plz-errors

--- app/src/main/java/com/example/denjae/androidprototype/MainActivity.java
+++ app/src/main/java/com/example/denjae/androidprototype/MainActivity.java
@@ -1,20 +1,29 @@
 package com.example.denjae.androidprototype;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
     ProgressBar progressBar;
     Button sendButton;
     TextView threat;
+    EditText cityInput;
+    EditText plzInput;
+    int plz;
+    String location;
 
 
     @Override
@@ -23,22 +32,26 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         findVIewsById();
         progressBar.setVisibility(View.INVISIBLE);
+        sendButton.setOnClickListener(this);
+
     }
 //    Initialisiert die Interaktionselemente
     private void findVIewsById() {
         this.progressBar = (ProgressBar) findViewById(R.id.progressBar);
         this.sendButton = (Button) findViewById(R.id.sendLocation);
         this.threat = (TextView) findViewById(R.id.threatLevelOutput);
+        this.cityInput = (EditText) findViewById(R.id.cityInput);
+        this.plzInput = (EditText) findViewById(R.id.plzInput);
     }
 
-
+    //Von IDE erstellt
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-
+    //Von IDE erstellt
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -51,14 +64,31 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void onClick(){
-        progressBar.setVisibility(View.VISIBLE);
-    }
 
+    //Methode zum Ermitteln des Bedrohungsgrades
     public int threatLevel(){
         int level = 0;
 
        return level;
     }
+    //Speichern der eingegebenen Daten beim Klicken des Senden-Buttons
+    @Override
+    public void onClick(View view) {
+        try{
+        plz = Integer.parseInt(plzInput.getText().toString());
+        }
+        catch (Exception e) {
+            new AlertDialog.Builder(this)
+                    .setMessage("error").setNeutralButton("test", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.dismiss();
+                }
+            }).show();
+        }
+        location = cityInput.getText().toString();
+        Log.w("debug", "location: " + location + ", plz:" + plz);
+    }
+
 
 }
