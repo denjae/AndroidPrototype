fa0e424641267d070b64eb922b8db149606d143f
denjae <dennis.jga@gmail.com>
2014-07-26 17:39:59 +0200

    search uri added

--- app/src/main/java/com/example/denjae/androidprototype/APIconnections.java
+++ app/src/main/java/com/example/denjae/androidprototype/APIconnections.java
@@ -6,8 +6,12 @@ package com.example.denjae.androidprototype;
 public class APIconnections {
     int foursquare;
     public static final String connection = "https://foursquare.com/oauth2/authenticate?client_id=BXBK3ZES42YG5KDEBCCFCOKZTYKZIP1LYZYXCJCGNO2ORTB5&response_type=code&redirect_uri=https://foursquare.com/oauth2/authenticate";
+    String venueURL;
+    String city;
 
-    public int getFoursquare(){
+    public int getFoursquare(String city){
+        this.city=city;
+        String venueURL= "https://api.foursquare.com/v2/venues/search?near="+city +"&client_id=BXBK3ZES42YG5KDEBCCFCOKZTYKZIP1LYZYXCJCGNO2ORTB5&client_secret=KE53YHPKFWUS4LJ5JLU1EFOKUPPDBFDFZWZINVBK0QMHIATA&v=20140726";
 
         return foursquare;
     }
