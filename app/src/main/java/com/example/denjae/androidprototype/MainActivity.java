7be8cbf3b4e0740c97031a10db201188683532b0
denjae <dennis.jga@gmail.com>
2014-08-03 21:37:20 +0200

    start develop the json-search. not working yet

--- app/src/main/java/com/example/denjae/androidprototype/MainActivity.java
+++ app/src/main/java/com/example/denjae/androidprototype/MainActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.util.JsonReader;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -17,20 +16,20 @@ import android.widget.TextView;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
 import java.util.concurrent.ExecutionException;
 
 
-public class MainActivity extends Activity implements View.OnClickListener{
+public class MainActivity extends Activity implements View.OnClickListener {
 
     ProgressBar progressBar;
     Button sendButton;
     TextView threat;
     EditText cityInput;
-    String result;
     int foursqareLevel;
     AsyncRequest asyncRequest;
     JSONObject json;
-    JSONArray jsonArray;
+    JSONObject jsonStats;
 
     //onCreate-Methode. Interaktionselemente werden initialisiert, ein Klick-Listener auf den Senden-Button gesetzt und der ProgressBar-Balken versteckt
     @Override
@@ -41,7 +40,6 @@ public class MainActivity extends Activity implements View.OnClickListener{
         sendButton.setOnClickListener(this);
 
         asyncRequest = new AsyncRequest(progressBar);
-
 
 
     }
@@ -82,8 +80,7 @@ public class MainActivity extends Activity implements View.OnClickListener{
     @Override
     public void onClick(View view) {
         try {
-            getJSONFromForsquare(cityInput.getText().toString());
-            threatFoursqure();
+            threatFoursqure(cityInput.getText().toString());
         } catch (Exception e) {
             e.printStackTrace();
             new AlertDialog.Builder(this).setMessage("Fehler bei der Verarbeitung der Daten").setNeutralButton("Erneut versuchen", new DialogInterface.OnClickListener() {
@@ -98,46 +95,21 @@ public class MainActivity extends Activity implements View.OnClickListener{
 
     }
 
-    //Ruft die Ergebnisse der eingegebenen Stadt von Foursquare ab und wertet diese in Form eines Integers aus
-    public void getJSONFromForsquare(String location){
-        String urlFoursquare = "https://api.foursquare.com/v2/venues/search?near=" + location + "&client_id=BXBK3ZES42YG5KDEBCCFCOKZTYKZIP1LYZYXCJCGNO2ORTB5&client_secret=KE53YHPKFWUS4LJ5JLU1EFOKUPPDBFDFZWZINVBK0QMHIATA&v=20140726";
-        asyncRequest.execute(urlFoursquare);
-       }
 
-
-    public int threatFoursqure () throws ExecutionException, InterruptedException, JSONException {
-        result= asyncRequest.get();
-        Log.d("debug","result" + result);
-        result = asyncRequest.get();
-        Log.d("debug", "result from asyncRequest" +result);
-        json = new JSONObject(result);
-        Log.d("debug", "jsonArray" +json);
+    public int threatFoursqure(String location) throws ExecutionException, InterruptedException, JSONException {
+        foursqareLevel=0;
+        String urlFoursquare = "https://api.foursquare.com/v2/venues/search?near=" + location + "&limit=3&novelty=new&client_id=BXBK3ZES42YG5KDEBCCFCOKZTYKZIP1LYZYXCJCGNO2ORTB5&client_secret=KE53YHPKFWUS4LJ5JLU1EFOKUPPDBFDFZWZINVBK0QMHIATA&v=20140726";
+        json = new JSONObject(asyncRequest.execute(urlFoursquare).get());
         for (int i = 0; i < json.length(); i++) {
-            Log.d("debug", "for-loop with i");
-            try {
-                jsonArray = new JSONArray(json.getString("stats"));
-            } catch (JSONException e1) {
-                e1.printStackTrace();
-            }
-            for (int j = 0; j < jsonArray.length(); j++) {
-                Log.d("debug", "for-loop with j");
-                try {
-                    foursqareLevel += jsonArray.getInt(j);
-                } catch (JSONException e1) {
-                    new AlertDialog.Builder(this).setMessage("Fehler bei der Verarbeitung des Arrays").setNeutralButton("Erneut versuchen", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialogInterface, int i) {
-                            dialogInterface.dismiss();
-                        }
-                    }).show();
-                }
-            }
+            Log.d("debug", "Loop stated");
+            Log.d("debug", "Created json object" + json);
+            foursqareLevel+= json.getInt("tipCount");
 
+           // foursqareLevel += json.getJSONObject("stats").getInt("tipCount");
+           // foursqareLevel += json.getJSONObject("stats").getInt("usersCount");
         }
 
-
         Log.d("debug", "Ermitteltes Level Foursquare " + foursqareLevel);
-
 
         return foursqareLevel;
     }
